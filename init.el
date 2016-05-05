9361b2feeef7e7f9f1188ba36cc08fb76f524779
Thomas Marek <ttmarek@outlook.com>
2016-05-04 23:34:30 -0400

    Changed the keybinding for opening files in emacs

--- init.el
+++ init.el
@@ -91,7 +91,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x f") 'helm-find-files)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-z") 'helm-select-action)
 (semantic-mode 1)
