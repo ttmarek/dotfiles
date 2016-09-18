4181171c3085c7c5b256b9728a515fab28dc97fa
Thomas Marek <ttmarek@outlook.com>
2016-09-18 12:23:56 -0400

    Add use-package, ivy, and swiper modes

--- init.el
+++ init.el
@@ -1,68 +1,126 @@
-;; MELPA
-;; http://melpa.org/#/getting-started (keep at the top of init)
-;; -------------------------------------------------------------------
 (require 'package)
 
+;; --------------------------------------------------
+;; Delete trailing whitespace on save
+(add-hook 'before-save-hook 'whitespace-cleanup)
+;; No tabs
+(setq-default indent-tabs-mode nil)
+;; Set folder for emacs backup files
+(setq backup-directory-alist `(("." . "~/.emacs-backup")))
+;; Maximize the emacs window on startup
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; Line Spacing
+(setq-default line-spacing 7)
+;; Don't show startup screen
+(setq inhibit-startup-screen t)
+;; Highlight matching parens
+(show-paren-mode 1)
+;; Enable line numbers
+(global-linum-mode t)
+;; Hide the toolbar
+(tool-bar-mode -1)
+;; Hide the scrollbar
+(toggle-scroll-bar -1)
+;; Hide the menubar
+(menu-bar-mode -1)
+;; Set font face and size
+(defun font-exists (font)
+  "check if font exists"
+  (if (null (x-list-fonts font)) nil t))
+
+(if (font-exists "inconsolata")
+    (set-face-attribute 'default nil :font "inconsolata" :height 140))
+
+;; Packages
+;; --------------------------------------------------
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 
 ;; Activate all the packages
 (package-initialize)
 
-;; Fetch the list of available packages
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-;; Packages
-;; -------------------------------------------------------------------
-(setq to-install-if-needed '(helm
-                             helm-mt
-                             magit
-                             json-mode
-                             yaml-mode
-                             scss-mode
-                             markdown-mode
-                             web-mode
-                             js2-mode
-                             aurora-theme
-                             smartparens ))
+(use-package css-mode
+  :init
+  (setq css-indent-offset 2))
 
-;; Loop through the list of packages and install any if they haven't
-;; been already
-(dolist (pkg to-install-if-needed)
-  (unless (package-installed-p pkg)
-    (package-install pkg)))
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t))
 
-;; RANDOM
-;; -------------------------------------------------------------------
-;; Delete trailing whitespace on save
-(add-hook 'before-save-hook 'whitespace-cleanup)
+(use-package smartparens
+  :ensure t
+  :config
+  (smartparens-global-mode t))
 
-;; NO TABS
-(setq-default indent-tabs-mode nil)
+(use-package magit
+  :ensure t
+  :bind
+  ("C-x g" . magit-status))
 
-;; Set folder for emacs backup files
-(setq backup-directory-alist `(("." . "~/.emacs-backup")))
+(use-package swiper
+  :ensure t
+  :bind
+  ("C-s" . swiper)
+  :init
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-display-style 'fancy)
+  :config
+  (ivy-mode 1))
 
-;; Maximize the emacs window on startup
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(use-package counsel
+  :ensure t
+  :bind
+  ("M-x" . counsel-M-x)
+  ("C-x C-f" . counsel-find-file))
 
-;; Line Spacing
-(setq-default line-spacing 6)
+(use-package multi-term
+  :ensure t
+  :bind
+  ("C-x t" . multi-term))
 
-;; Random Functions
-(defun copy-file-path()
-  "Put the current file name on the clipboard"
-  (interactive)
-  (let ((filename (if (equal major-mode 'dired-mode)
-                      default-directory
-                    (buffer-file-name))))
-    (when filename
-      (with-temp-buffer
-        (insert filename)
-        (clipboard-kill-region (point-min) (point-max)))
-      (message filename))))
+(use-package web-mode
+  :ensure t
+  :mode "\\.js\\'"
+  :init
+  (setq web-mode-content-type "jsx")
+  (setq web-mode-enable-auto-quoting nil)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2)
+  :config
+  (js2-minor-mode))
 
+(use-package js2-mode
+  :ensure t
+  :init
+  (setq js2-global-externs
+        '("describe"
+          "it"
+          "expect"
+          "beforeEach"
+          "afterEach"
+          "jest"))
+  (setq js2-strict-trailing-comma-warning nil)
+  (setq js2-include-node-externs t))
+
+(use-package json-mode
+  :ensure t
+  :init
+  (make-local-variable 'js-indent-level)
+  (setq js-indent-level 2))
+
+(use-package markdown-mode
+  :ensure t
+  :mode "\\.md\\'")
+
+;; Custom Functions
+;; --------------------------------------------------
 (defun rename-file-and-buffer ()
   "Rename the current buffer and file it is visiting."
   (interactive)
@@ -76,113 +134,14 @@
           (rename-file filename new-name t)
           (set-visited-file-name new-name t t)))))))
 
-;; SMARTPARENS
-;; -------------------------------------------------------------------
-(smartparens-global-mode t)
-
-;; YAML
-;; -------------------------------------------------------------------
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-hook 'yaml-mode-hook
-    '(lambda ()
-       (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
-
-;; SASS
-;; -------------------------------------------------------------------
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-
-;; HELM
-;; -------------------------------------------------------------------
-(helm-mode 1)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(setq helm-ff-newfile-prompt-p nil)
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-z") 'helm-select-action)
-(semantic-mode 1)
-
-;; Multiple Terminal Management
-;; -------------------------------------------------------------------
-(global-set-key (kbd "C-x t") 'helm-mt)
-
-;; Highlight Matching Parenthesis
-;; -------------------------------------------------------------------
-(show-paren-mode 1)
-
-;; JavaScript
-;; -------------------------------------------------------------------
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-
-(defun js2-mode-hooks ()
-  "Hooks for js2-mode"
-  ;; enable nodejs syntax highlighting
-  (setq js2-include-node-externs t)
-  ;; enable custom variable highlighting
-  (setq js2-global-externs
-        '("describe" "it" "expect" "beforeEach" "afterEach" "jest"))
-  ;; disable trailing comma warning
-  (setq js2-strict-trailing-comma-warning nil))
-
-(add-hook 'js2-minor-mode-hook 'js2-mode-hooks)
-
-(defun web-mode-hooks ()
-  "Hooks for web-mode"
-  ;; enable js2 minor mode
-  (js2-minor-mode)
-  ;; enable JSX support
-  (setq web-mode-content-type "jsx")
-  ;; disable auto-quote attributes
-  (setq web-mode-enable-auto-quoting nil)
-  ;; set indentation
-  (setq web-mode-code-indent-offset 2)
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-attr-indent-offset 2))
-
-(add-hook 'web-mode-hook 'web-mode-hooks)
-
-;; CSS
-;; -------------------------------------------------------------------
-(defun css-mode-hooks ()
-  "Hooks for css-mode"
-  (setq css-indent-offset 2))
-
-;; JSON
-;; -------------------------------------------------------------------
-(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
-
-(add-hook 'json-mode-hook
-          (lambda ()
-            (make-local-variable 'js-indent-level)
-            (setq js-indent-level 2)))
-
-;; MARKDOWN
-;; -------------------------------------------------------------------
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;; APPEARANCE
-;; -------------------------------------------------------------------
-(setq inhibit-startup-screen t)
-
-(global-linum-mode t)                   ; enable line #s
-
-(tool-bar-mode -1)                      ; hide toolbar
-
-(toggle-scroll-bar -1)                  ; hide scrollbar
-
-(menu-bar-mode -1)                      ; hide menubar
-
-;; Set font face and size
-(defun font-exists (font)
-  "check if font exists"
-  (if (null (x-list-fonts font)) nil t))
-
-(if (font-exists "inconsolata")
-    (set-face-attribute 'default nil :font "inconsolata" :height 140))
-
-(load-theme 'aurora t)
+(defun copy-file-path()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
