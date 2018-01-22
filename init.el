b8d297e747bae0b303ae9ca0b9ff08c8f75ce9fa
Thomas Marek <thomas.marek@bnc.ca>
2018-01-22 17:07:35 -0500

    Make counsel-ag work

--- init.el
+++ init.el
@@ -65,6 +65,12 @@
   :init
   (add-hook 'css-mode-hook 'rainbow-mode))
 
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
 (use-package counsel-projectile
   :ensure t
   :init
